0a4c58377b03f69f67dfc2879d44ce7adeb6f5b3
CodingPerson786 <h.miah@lancaster.ac.uk>
2023-02-20 14:25:35 +0000

    latest RunCarGame

--- RunCarGame.java
+++ RunCarGame.java
@@ -20,29 +20,29 @@ public class RunCarGame
         baddies[1]= new Baddie(300,500);
         baddies[2]= new Baddie(450,500);
 
-        if(baddies[0].carCollide)
-        {
+        //if(baddies[0].carCollide)
+        //{
             //game ends
-        }
+        //}
 
-        if (baddies[0].xPosition== 0)
+        if (baddies[0].getXPosition()== 0)
         {
             baddies[0].setXPosition(750);
             baddies[0].setYPosition(450);
         }
 
-        if (baddies[1].xPosition== 0)
+        if (baddies[1].getXPosition()== 0)
         {
             baddies[1].setXPosition(300);
             baddies[1].setYPosition(500);
         }
 
-        if (baddies[2].xPosition== 0)
+        if (baddies[2].getXPosition()== 0)
         {
             baddies[2].setXPosition(450);
             baddies[2].setYPosition(500);
         }
-        while(baddies[0].xPosition!=-150)
+        while(baddies[0].getXPosition()!=-150)
         {
             baddies[0].moveBaddie(-1);//moves left one
         }
